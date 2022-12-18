b3e920f4e7580670858329f34111cb3e50f31b25
Will Crichton <wcrichto@cs.stanford.edu>
2022-12-18 00:30:13 +0000

    Initial commit

---
+++ crates/docsrs-scrape-examples-test/examples/ex.rs
@@ -0,0 +1,3 @@
+fn main() {
+  docsrs_scrape_examples_test::add(1, 2);
+}
---
+++ crates/docsrs-scrape-examples-test/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn incr(x: usize) -> usize {
+  add(x, 1)
+}
